9b17bc9bd4c0093052a5ab467a3c9c151c86743a
Mustafa Muhammad <mustafa.m257@proton.me>
2024-02-26 02:08:49 -0500

    Work on auth card in authentication page

---
+++ pages/authentication.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="h-screen grid place-content-center">
+    <div class="container">
+      <div class="card">
+        <div class="flex justify-center">
+          <div class="flex justify-center w-20 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+              />
+            </svg>
+          </div>
+        </div>
+        <button class="btn-primary py-3 w-full rounded-full">
+          Continue with github
+        </button>
+        <hr class="border-white/10 my-8" />
+        <form>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              class="input"
+              placeholder="mustafa.m257@proton.me"
+            />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              class="input"
+              placeholder="supersecret"
+            />
+          </div>
+          <button class="btn-primary py-3 w-full rounded-full mt-6">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,18 @@
       <h1 class="text-5xl font-bold heading">
         Unleash the Power of Every Click with Linkique: Shorten, Share, & Shine
       </h1>
+      <p class="my-10 text-lg md:w-2/3 mx-auto text-center">
+        Transform your links into short, memorable pathways with Linkique.
+      </p>
+      <NuxtLink to="/" class="btn btn-primary">Shorten Now</NuxtLink>
     </div>
   </section>
 </template>
+<style scoped>
+.heading {
+  background: linear-gradient(180deg, #fff 0%, rgba(255, 255, 255, 0) 180%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
