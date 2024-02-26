47ec0b107621d17b8956ead8e8edcf0646cd353a
Mustafa Muhammad <mustafa.m257@proton.me>
2024-02-26 01:36:32 -0500

    Finalize navbar

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -1,15 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const links = ref<
+  {
+    to: string;
+    label: string;
+  }[]
+>([
+  {
+    to: "/",
+    label: "Home",
+  },
+  {
+    to: "/about",
+    label: "About",
+  },
+  {
+    to: "/contact",
+    label: "Contact",
+  },
+]);
+</script>
 
 <template>
-  <div class="fixed top-0 left-0 right-0">
-    <nav class="container py-4">
+  <div class="fixed top-0 left-0 right-0 border-white/20 bg-blur">
+    <nav class="container py-4 flex justify-between">
       <NuxtLink
+        class="text-2xl font-bold text-white"
         :to="{
           name: 'index',
         }"
       >
         Linkique
       </NuxtLink>
+      <ul class="flex items-center gap-4">
+        <li v-for="link in links" :key="link.to">
+          <NuxtLink :to="link.to">{{ link.label }}</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/auth" class="btn btn-primary">Sign in</NuxtLink>
+        </li>
+      </ul>
     </nav>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,9 @@
-<template></template>
+<template>
+  <section class="h-screen grid place-content-center">
+    <div class="max-w-3xl mx-auto text-center">
+      <h1 class="text-5xl font-bold heading">
+        Unleash the Power of Every Click with Linkique: Shorten, Share, & Shine
+      </h1>
+    </div>
+  </section>
+</template>
