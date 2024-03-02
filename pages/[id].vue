7c1bbf8d9f53e0f0c525f66b28c1f525cd0ebe7e
Mustafa Muhammad <mustafa.m257@proton.me>
2024-03-02 01:26:14 -0500

    Some ts bugs & optimzation to be fixed NEXT

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,38 +1,44 @@
 <script setup lang="ts">
 const params = useRoute().params;
-// from the params ID, I'll fetch teh data from supabase & redirect to the long URL
-
-if (!params.id) {
-  throw createError({
-    statusCode: 404,
-    message: "Not Found",
-  });
-}
 const client = useSupabaseClient();
+const data = ref(null);
 
-const { data } = useAsyncData("link", async () => {
-  const { data, error } = await client
+// Wrap async operations in a function
+async function fetchDataAndRedirect() {
+  if (!params.id) {
+    throw createError({
+      statusCode: 404,
+      message: "Not Found",
+    });
+  }
+
+  const { data: linkData, error } = await client
     .from("links")
     .select("*")
     .eq("key", params.id)
     .single();
-  if (error) {
-    throw error;
-  }
-  return data;
-});
 
-if (data.value?.long_url) {
-  useRedirect(data.value.long_url);
+  if (error) throw error;
+
+  data.value = linkData;
+
+  if (linkData?.long_url) {
+    useRedirect(linkData.long_url);
+  }
 }
-// Redirect to the long url
+
+onMounted(() => {
+  fetchDataAndRedirect().catch((error) => {
+    console.error(error);
+  });
+});
 </script>
 <template>
   <div>
     <h1>Redirecting...</h1>
-  </div>
-  <div class="pt-[200px]">
-    {{ params }}
-    {{ data }}
+    <div class="pt-[200px]">
+      {{ params }}
+      {{ data }}
+    </div>
   </div>
 </template>
